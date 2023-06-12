3b4ce1cc861856d03c1b3d424281753b00eb286c
Kemal <outsider.pld@gmail.com>
2023-06-13 00:42:09 +0100

    fixed minor issue with env variables

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+
+start();
+console.log();
+async function start() {
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.DB_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log("Database ready");
+  } catch (error) {
+    console.log(error.message);
+    console.error("Database connection failed");
+  }
+
+  mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected");
+  });
+
+  mongoose.connection.on("connected", () => {
+    console.log("mongoDB conntected");
+  });
+
+  const app = express();
+  app.use(express.json());
+
+  app.get("/", (req, res) => {
+    res.json({ message: "REST service operational" });
+  });
+
+  const server = app.listen(PORT, () =>
+    console.log(`REST service started on ${PORT}`)
+  );
+}
