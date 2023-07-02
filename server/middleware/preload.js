d50de0de4dc9ccbe9c8827885a5624a270b5942c
Kemal <outsider.pld@gmail.com>
2023-07-02 16:49:52 +0100

    minor fix - preload minor changes - searches service
    added
    - CatalogService to featured and Details

--- server/middleware/preload.js
+++ server/middleware/preload.js
@@ -6,6 +6,7 @@ module.exports = () => async (req, res, next) => {
     const item = await getById(id);
     item._ownerId = item.owner;
     res.locals.item = item;
+    next();
   } catch (error) {
     res.status(404).json({ message: "Record not found" });
   }
