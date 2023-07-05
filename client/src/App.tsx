a3dda83c7a412de4609247a86b1231be114d709b
Kemal <outsider.pld@gmail.com>
2023-07-05 19:46:18 +0100

    created - login component with some html

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Catalog from "./components/Catalog/Catalog";
 import Details from "./components/Details/Details";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
@@ -11,8 +12,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/catalog" element={<Catalog/>}/>
-        <Route path="/catalog/:itemId" element={<Details />}/>
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/catalog/:itemId" element={<Details />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
     </div>
--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -39,30 +39,6 @@ const Navbar = (props: Props) => {
                     Catalogue
                   </Link>
                 </div>
-                {/* <div className="relative hidden md:block ">
-                  <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                    <svg
-                      className="h-6 w-6 text-black"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                        clip-rule="evenodd"
-                      ></path>
-                    </svg>
-                    <span className="sr-only">Search icon</span>
-                  </div>
-                  <input
-                    type="text"
-                    id="search-navbar"
-                    className="block w-full rounded-full border bg-opacity-30 backdrop-blur-lg backdrop-filter border-black bg-white p-3 pl-10 text-lg text-black placeholder:align-bottom placeholder:text-xl focus:border-emerald-400 focus:ring-emerald-400"
-                    placeholder="Search..."
-                  />
-                </div> */}
                 <div className="flex items-center justify-between gap-8">
                   <Link
                     className="text-xl font-semibold transition duration-500 hover:text-emerald-600"
@@ -79,30 +55,6 @@ const Navbar = (props: Props) => {
               </div>
             ) : (
               <>
-                {/* <div className="relative hidden md:block">
-                  <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 mr-5">
-                    <svg
-                      className="h-6 w-6 text-black"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                        clip-rule="evenodd"
-                      ></path>
-                    </svg>
-                    <span className="sr-only">Search icon</span>
-                  </div>
-                  <input
-                    type="text"
-                    id="search-navbar"
-                    className="block w-full rounded-full border border-black bg-white p-3 pl-10 text-lg text-black  placeholder:text-2xl focus:border-emerald-400 focus:ring-emerald-400"
-                    placeholder="Search..."
-                  />
-                </div> */}
                 <button
                   className="rounded-full bg-emerald-400 p-2"
                   onClick={() => setIsMenuToggled(!isMenuToggled)}
