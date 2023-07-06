885d0aa05da50095d047a1f9844a1460eb127c33
Kemal <outsider.pld@gmail.com>
2023-07-06 19:46:02 +0100

    created - useLocalStorage hook and userService

---
+++ client/src/hooks/useLocalStorage.ts
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export const useLocalStorage = (key: string, defaultValue: string) => {
+  const [value, setValue] = useState(() => {
+    const storedData = localStorage.getItem(key);
+
+    return storedData ? JSON.parse(storedData) : defaultValue;
+  });
+
+  const setLocalStorageValue = (newValue: string) => {
+    localStorage.setItem(key, JSON.stringify(newValue));
+
+    setValue(newValue);
+  };
+
+  return [value, setLocalStorageValue];
+};
