ce7e50d6d44abaac610bf512d136bf65bcd5c82b
Kemal <outsider.pld@gmail.com>
2023-07-14 23:48:16 +0100

    fixed - createOrUpdateSearches not passing domain
    to be fixed
    - pagination

---
+++ client/src/components/Pagination/Pagination.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {
+  itemsPerPage: number;
+  totalItems: number;
+  paginate: (pageNumber: number) => void;
+};
+
+const Pagination = (props: Props) => {
+  return <div className="flex gap-3 flex-wrap p-6 py-12">
+    
+  </div>
+};
+
+export default Pagination;
